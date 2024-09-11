e98a8d62e9db8ea442bdeea6e5e0c7a198d8d9ee
Trntybth <trinitybooth89@gmail.com>
2024-09-11 17:16:25 +0100

    feat: added functionality to getAlbumById in controller class

--- src/main/java/Controller/AlbumController.java
+++ src/main/java/Controller/AlbumController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/records")
@@ -21,5 +22,11 @@ public class AlbumController {
         List<Album> records = albumManagerService.getAllAlbums();
         return new ResponseEntity<>(records, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public Optional<Album> getAlbumById(@PathVariable Long id) {
+        return albumManagerService.getAlbumById(id);
+    }
+
 
 }
