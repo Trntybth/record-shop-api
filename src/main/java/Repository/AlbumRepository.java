ed95e9c6b74cc063b997bb927f768467613ba2ef
Trntybth <trinitybooth89@gmail.com>
2024-09-11 16:28:45 +0100

    feat: added AlbumRepository to newly created repository layer

---
+++ src/main/java/Repository/AlbumRepository.java
@@ -0,0 +1,15 @@
+package Repository;
+
+import model.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+}
+
+
+
+
