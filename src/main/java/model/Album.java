409a6a51c628872ba9736ad916d131b76b8ca086
Trntybth <trinitybooth89@gmail.com>
2024-09-11 16:22:02 +0100

    feat: created model package containing enum Genre and @Entity class Album

---
+++ src/main/java/model/Album.java
@@ -0,0 +1,27 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "album")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String artist;
+    private int releaseYear;
+    private Genre genre;
+
+}
