547a10cf7baed091bdd59de7d9d335dded1098d0
Trntybth <trinitybooth89@gmail.com>
2024-09-11 16:44:56 +0100

    feat: added injection of AlbumManagerService instance to AlbumController class, and added functionality to getAll()

--- src/main/java/Controller/AlbumController.java
+++ src/main/java/Controller/AlbumController.java
@@ -1,10 +1,25 @@
 package Controller;
 
+import Service.AlbumManagerService;
+import model.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/records")
 public class AlbumController {
 
+    @Autowired
+    private AlbumManagerService albumManagerService;
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAll() {
+        List<Album> records = albumManagerService.getAllAlbums();
+        return new ResponseEntity<>(records, HttpStatus.OK);
+    }
 
 }
